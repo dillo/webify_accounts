282af2f1c7abe526d312ea6ffe68b8b8a58a0579
Dillo Raju <dilloshion@gmail.com>
2021-07-12 07:34:12 -0400

    1 commenting out default sidekiq in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,8 @@ Rails.application.configure do
   }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  config.active_job.queue_adapter     = :resque
-  config.active_job.queue_name_prefix = '_change_me_production'
+  # config.active_job.queue_adapter     = :resque
+  # config.active_job.queue_name_prefix = '_change_me_production'
 
   config.action_mailer.perform_caching = false
 
