8ba61f33603770aaed65b8071314e5a34df1b6b6
Dillo Raju <dilloshion@gmail.com>
2021-07-19 20:56:43 -0400

    Setup prometheus_exporter - Instrument sql/redis/total-time - Instrument per process stats - Instrument sidekiq stats

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,6 +5,12 @@ Sidekiq.configure_server do |config|
     host: ENV['REDIS_HOST'],
     port: ENV['REDIS_PORT'] || '6379'
   }
+
+  config.server_middleware do |chain|
+    require 'prometheus_exporter/instrumentation'
+
+    chain.add PrometheusExporter::Instrumentation::Sidekiq
+ end
 end
 
 Sidekiq.configure_client do |config|
